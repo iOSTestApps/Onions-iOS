4c629255cda9da89e6617d6427a048c1561d88c6
Christopher Stott <christopherstott@gmail.com>
2015-08-11 08:27:48 -0700

    logging in prebuild script

--- buddybuild_prebuild.h
+++ buddybuild_prebuild.h
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+echo INSIDE PREBUILD SCRIPT
+echo `pwd`
+echo hello > test.txt
 echo "#define PARSE_APP_ID @\"YOUR_PARSE_APP_ID_KEY\"" > ./Onions-iOS/OCParseConstants.h
 echo "#define PARSE_CLIENT_ID @\"YOUR_PARSE_CLIENT_ID_KEY\"" >> ./Onions-iOS/OCParseConstants.h
