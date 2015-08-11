1fd49fa9b657d8af15d6e73fc5d18da253f4d39a
Christopher Stott <christopherstott@gmail.com>
2015-08-11 08:16:49 -0700

    Adding buddybuild_prebuild.h

---
+++ buddybuild_prebuild.h
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "#define PARSE_APP_ID @\"YOUR_PARSE_APP_ID_KEY\"" > ./Onions-iOS/OCParseConstants.h
+echo "#define PARSE_CLIENT_ID @\"YOUR_PARSE_CLIENT_ID_KEY\"" >> ./Onions-iOS/OCParseConstants.h
